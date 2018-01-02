e70dc82f6f4bb892ffc133dc36115c6f38b92daf
Vikas Nair <vikasnair@vikaaaaaaaaas.local>
2018-01-02 16:25:12 -0500

    second commit, objectified for any city/town

--- zigzag.py
+++ zigzag.py
@@ -1,17 +1,29 @@
-from urllib.request import urlopen
+from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+import requests
 import json
 
-def find_agents():
-	
+def find_agents(city, town):
+	url = "https://www.wavemoney.com.mm/closest-wave-shop/?lang=&city=" + city + "&town=" + town + "#search-store"
 
+	# url = "https://d3vv6lp55qjaqc.cloudfront.net/items/2W0z3y431R0n0306281t/permalinkhttpswwwwavemoneycomm.txt?X-CloudApp-Visitor-Id=1437757"
+
+	# req = urlopen(url)
+	req = requests.get(url)
+	data = req.text
+	soup = BeautifulSoup(data, "html.parser")
+	script = soup.find_all('script')[-1]
+	beg = script.string.index("markers = initMarkers(map, ") + 27
+	end = beg + script.string[beg:].index(");")
+	markers = script.string[beg : end - 4] + "]"
+
+	data = json.loads(markers.replace(":\"", "\":\"").replace("\", ", "\", \"").replace("{ ", "{ \"").replace("),", ")\"").replace(": ", "\": \""))
+
+	for i in data:
+		print(i, "\n")
 
 def main():
-	# url = "https://www.wavemoney.com.mm/wp-json/contact-form-7/v1"
+	find_agents("yangon", "north-okkalapa")
 
-	url = "https://d3vv6lp55qjaqc.cloudfront.net/items/2W0z3y431R0n0306281t/permalinkhttpswwwwavemoneycomm.txt?X-CloudApp-Visitor-Id=1437757"
-	req = urlopen(url)
-	data = json.loads(req.read().decode().replace(":\"", "\":\"").replace("\", ", "\", \"").replace("{ ", "{ \"").replace("),", ")\"").replace(": ", "\": \""))
-
-	
 
 main()
